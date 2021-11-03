27a766515ed6721f4485609e3e0cda2685720480
pdjember <93108345+pdjember@users.noreply.github.com>
2021-11-03 10:25:46 +0700

    Add files via upload

---
+++ application/views/auth/registration.php
@@ -0,0 +1,59 @@
+<div class="container">
+
+  <div class="card o-hidden border-0 shadow-lg my-5 col-lg-6 mx-auto">
+    <div class="card-body p-0">
+      <!-- Nested Row within Card Body -->
+      <div class="row">
+        <div class="col-lg">
+          <div class="p-5">
+            <div class="text-center">
+              <img src="<?= base_url('assets/img/icon/BUNGA SEPASANG.png') ?>" class="mb-4" height="155" width="195">
+              <h1 class="h4 text-gray-900 mb-4">Registrasi Akun Anda</h1>
+            </div>
+            <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
+
+              <div class="form-group">
+                <input type="text" class="form-control form-control-user" id="nama" name="nama" placeholder="Nama lengkap" value="<?= set_value('nama'); ?>">
+                <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
+              </div>
+
+              <div class="form-group">
+                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Alamat Email" value="<?= set_value('email'); ?>">
+                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+              </div>
+
+              <div class="input-group mb-3">
+                <select class="custom-select" id="unit" name="unit">
+                  <option selected>Pilih unit...</option>
+                  <?php foreach ($unit as $u) : ?>
+                    <option value="<?= $u['id']; ?>"><?= $u['nama_unit']; ?></option>
+                  <?php endforeach; ?>
+                </select>
+                <?= form_error('unit', '<small class="text-danger pl-3">', '</small>'); ?>
+              </div>
+
+              <div class="form-group row">
+                <div class="col-sm-6 mb-3 mb-sm-0">
+                  <input type="password" class="form-control form-control-user" id="pass1" name="pass1" placeholder="Password">
+                  <?= form_error('pass1', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <div class="col-sm-6">
+                  <input type="password" class="form-control form-control-user" id="pass2" name="pass2" placeholder="Ulangi Password">
+                </div>
+              </div>
+
+
+
+              <button type="submit" class="btn btn-primary btn-user btn-block">Daftar</button>
+            </form>
+            <hr>
+            <div class="text-center">
+              <a class="small" href="<?= base_url('auth'); ?>">Sudah Punya Akun? Login!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
